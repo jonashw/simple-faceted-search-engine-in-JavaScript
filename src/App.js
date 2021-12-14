73b465875562c4721806341c3988f7aaa7fba548
Jon Wilson <2jonwilson@gmail.com>
2021-12-14 14:03:50 -0800

    move settings to a separate tab with routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import "./styles.css";
 import FacetedIndex from "./FacetedIndex";
 import records from "./Records";
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
 
 export default function App() {
   const [formJsonData, setFormJsonData] = React.useState("");
@@ -15,16 +22,7 @@ export default function App() {
     setFormJsonData(JSON.stringify(ix.records, null, 2));
     setFormJsonConfig(JSON.stringify(ix.config, null, 2));
   }, [ix]);
-  const changeData = () => {
-    var data = JSON.parse(formJsonData);
-    var cfg = JSON.parse(formJsonConfig);
 
-    if (!data || !Array.isArray(data) || !cfg) {
-      alert("sorry, invalid data");
-    } else {
-      setIx(FacetedIndex(data, cfg));
-    }
-  };
   const [query, setQuery] = React.useState({});
 
   const searchResult = ix.search(query);
@@ -43,48 +41,63 @@ export default function App() {
     setQuery(newQuery);
   };
 
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-3">
-          {searchResult.facets
-            .filter((f) => f.term_buckets.length > 0)
-            .map(({ facet_id, term_buckets }) => (
-              <div className="mb-3">
-                <h4>{facet_id}</h4>
-                {term_buckets.map((t) => (
-                  <div onClick={() => toggleSearchTerm(facet_id, t.term)}>
-                    <input type="checkbox" checked={t.in_query} /> {t.term} (
-                    {t.count})
-                  </div>
-                ))}
-              </div>
-            ))}
-          <pre>{JSON.stringify(searchResult.query, null, 2)}</pre>
-        </div>
-        <div className="col-9">
-          {searchPerformed && (
-            <button
-              className="btn float-end btn-link btn-sm"
-              onClick={() => setQuery({})}
-            >
-              Reset
-            </button>
-          )}
-          <h5>Results: {searchResult.records.length}</h5>
-          {searchResult.records.map((r) => (
-            <div className="card mb-3">
-              <div className="card-body">
-                <div className="card-text">
-                  <pre>{JSON.stringify(r, null, 2)}</pre>
+  const Search = ({}) => (
+    <div className="row">
+      <div className="col-3">
+        {searchResult.facets
+          .filter((f) => f.term_buckets.length > 0)
+          .map(({ facet_id, term_buckets }) => (
+            <div className="mb-3">
+              <h4>{facet_id}</h4>
+              {term_buckets.map((t) => (
+                <div onClick={() => toggleSearchTerm(facet_id, t.term)}>
+                  <input type="checkbox" checked={t.in_query} /> {t.term} (
+                  {t.count})
                 </div>
-              </div>
+              ))}
             </div>
           ))}
-        </div>
+        <pre>{JSON.stringify(searchResult.query, null, 2)}</pre>
+      </div>
+      <div className="col-9">
+        {searchPerformed && (
+          <button
+            className="btn float-end btn-link btn-sm"
+            onClick={() => setQuery({})}
+          >
+            Reset
+          </button>
+        )}
+        <h5>Results: {searchResult.records.length}</h5>
+        {searchResult.records.map((r) => (
+          <div className="card mb-3">
+            <div className="card-body">
+              <div className="card-text">
+                <pre>{JSON.stringify(r, null, 2)}</pre>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  const Settings = ({ setIx }) => {
+    const changeData = () => {
+      var data = JSON.parse(formJsonData);
+      var cfg = JSON.parse(formJsonConfig);
+
+      if (!data || !Array.isArray(data) || !cfg) {
+        alert("sorry, invalid data");
+      } else {
+        setIx(FacetedIndex(data, cfg));
+      }
+    };
+    return (
+      <div>
         <div className="row">
           <div className="col-6">
-            <label>Records data</label>
+            <label className="form-label">Records</label>
             <textarea
               className="form-control"
               rows={20}
@@ -94,7 +107,7 @@ export default function App() {
             ></textarea>
           </div>
           <div className="col-6">
-            <label>Index Config</label>
+            <label className="form-label">Config</label>
             <textarea
               className="form-control"
               rows={20}
@@ -113,6 +126,35 @@ export default function App() {
           </button>
         </div>
       </div>
-    </div>
+    );
+  };
+
+  return (
+    <Router>
+      <div className="container-fluid">
+        <nav className="my-3">
+          <ul className="nav nav-tabs">
+            {[
+              ["Search", "/"],
+              ["Settings", "/settings"]
+            ].map(([label, path]) => (
+              <li className="nav-item">
+                <NavLink
+                  activeClassName="active"
+                  className="nav-link"
+                  to={path}
+                >
+                  {label}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/settings" element={<Settings setIx={setIx} />} />
+          <Route path="/" element={<Search />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
