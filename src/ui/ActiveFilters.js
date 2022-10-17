a3d5d3f3687ee0f3ffc647f06959a5b0fefd6262
jonashw <jonwilson@ucanr.edu>
2022-10-17 12:21:07 -0700

    s/"Reset Search"/"Remove Filters" for clarity, plus better UX through compact layout

--- src/ui/ActiveFilters.js
+++ src/ui/ActiveFilters.js
@@ -1,27 +1,29 @@
 import React from "react";
 import './ActiveFilters.css';
 
-const ActiveFilters = ({query,setQuery,ix}) =>  
-    <div className="d-flex justify-content-between">
+const ActiveFilters = ({query,setQuery,ix}) =>  {
+    const facets = Object.entries(query);
+    return facets.length === 0 ? <></> : <div className="d-flex justify-content-between mb-3">
         <div>
-            {Object.entries(query).flatMap(([facetId, terms]) =>
-                terms.map(term => (
+            {facets.flatMap(([facetId, terms]) =>
+                terms.map((term,i) => (
                     <button className="btn btn-outline-success me-2" onClick={() => {
                         let nextQuery = ix.toggleQueryTerm(query, facetId, term);
                         setQuery(nextQuery);
-                    }}>
+                    }} key={i}>
                         {facetId}: {term}
                         <span className="btn-close ms-2" />
                     </button>
-                )))}
+            )))}
+            {facets.length > 1 && <button
+                className="btn btn-link"
+                disabled={Object.keys(query).length === 0}
+                onClick={() => setQuery({})}
+            >
+                Remove Filters
+            </button>}
         </div>
-        <button
-            className="btn btn-link flex-shrink-0 align-self-start"
-            disabled={Object.keys(query).length === 0}
-            onClick={() => setQuery({})}
-        >
-            Reset Search
-        </button>
     </div>;
+}
 
 export default ActiveFilters;
